3a03447842af8fa4d3d68172cd64773ad253fc36
YatoPythonGod <101020657+YatoPythonGod@users.noreply.github.com>
2023-01-16 11:11:40 +0300

    Update Eggs.java

--- src/Task_1/Eggs.java
+++ src/Task_1/Eggs.java
@@ -2,19 +2,20 @@ package Task_1;
 
 import java.util.Date;
 
-public class Eggs extends Food{
+public class Eggs extends Food {
 
     int piecesPerPack;
+
     public Eggs(String name, double price, int count, String unit, Date storageLife, int piecesPerPack) {
         super(name, price, count, unit, storageLife);
         this.piecesPerPack = piecesPerPack;
     }
 
-    public int getPiecesPerPack(){
+    public int getPiecesPerPack() {
         return this.piecesPerPack;
     }
 
-    public void setPiecesPerPack(int piecesPerPack){
+    public void setPiecesPerPack(int piecesPerPack) {
         this.piecesPerPack = piecesPerPack;
     }
 
